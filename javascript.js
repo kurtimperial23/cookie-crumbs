48ceeaecf5e7bb03c5d9695490c1dc2bf5a222b0
Kurt Imperial <76236273+kurtimperial23@users.noreply.github.com>
2023-05-29 10:22:50 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,28 @@
+$(".slider").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1500,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: "40px",
+                slidesToShow: 3,
+            },
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: "40px",
+                slidesToShow: 1,
+            },
+        },
+    ],
+});
